d905e63c72352079b28a1237cca9fb6ba31c39ea
Mohan <kmr0877@gmail.com>
2017-10-24 00:57:39 +1100

    updated fps file

---
+++ frames_per_sec.py
@@ -0,0 +1,38 @@
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", required=True,
+	help="/sample_test.mp4")
+args = vars(ap.parse_args())
+
+stream = cv2.VideoCapture(args["video"])
+fps = FPS().start()
+
+while True:
+	(grabbed, frame) = stream.read()
+ 
+	if not grabbed:
+		break
+	frame = imutils.resize(frame, width=450)
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	frame = np.dstack([frame, frame, frame])
+ 
+	#cv2.putText(frame, "Slow Method", (10, 30),
+		#cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)	
+ 
+	# show the frame and update the FPS counter
+	cv2.imshow("Frame", frame)
+	cv2.waitKey(1)
+	fps.update()
+
+fps.stop()
+print("elasped time: {:.2f}".format(fps.elapsed()))
+print("approx. FPS: {:.2f}".format(fps.fps()))
+ 
+
+stream.release()
+cv2.destroyAllWindows()
